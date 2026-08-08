975e68a3434ea369f49b9a7666bb69fe943c5c1c
Muhammad Sheeraz <sherazramzan61@gmail.com>
2026-08-08 20:31:18 +0500

    admin: add workshop abc-2000

--- config/workshops.ts
+++ config/workshops.ts
@@ -80,6 +80,19 @@ export const WORKSHOPS: WorkshopDefinition[] = [
     layout: DEFAULT_LAYOUT_CONFIG,
   },
 
+    {
+    key: "abc-2000",
+    workshopName: "Test Test",
+    workshopFullTitle:
+      "Test TestTest TestTest TestTest Test",
+    workshopCode: "ABC-2000",
+    eventYear: "2026",
+    eventDate: "12 Dec 2028",
+    organizedBy: `${ORG_CONFIG.organizationName} (${ORG_CONFIG.institutionAbbreviation})`,
+    templatePath: "/templates/abc-2000.png",
+    layout: DEFAULT_LAYOUT_CONFIG,
+  },
+
   // --- EXAMPLE: duplicate & fill in for your next workshop -----------------
   // {
   //   key: "aw-2026",
